1ba1018d0ba9885b26abe467f3a5c8c37b7218d5
Vigneshwaran Kannan <kvigneshwaranit@gmail.com>
2020-09-14 05:32:10 +0530

    added router and basic css

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,29 +1,40 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import Helmet from 'react-helmet';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
 import Header from './components/Header';
-import Pod from './sharedComponents/Pod';
+import Home from './modules/Home';
+import Product from './modules/Product';
 
 import './App.css';
-import Placeholder from './sharedComponents/Placeholder';
-import SharedModal from './sharedComponents/SharedModal';
-import SearchBar from './components/SearchBar';
-import SortBy from './components/SortBy';
-import Filter from './components/Filter';
-
 
 function App() {
-  return (
-    <div className="App">
-      Hello
-      <Header />
-      <Pod/>
-      <Placeholder />
-      <SharedModal />
-      <SearchBar />
-      <SortBy />
-      <Filter />
-    </div>
-  );
+	return (
+		<Fragment>
+			<Helmet>
+				<html lang='en' />
+				<meta charSet='utf-8' />
+				<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+				<meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=no' />
+				<meta name='msapplication-TileColor' content='#2b2b2b' />
+				<title>ABCD Gold Shop</title>
+			</Helmet>
+			<div className='App'>
+				<Header />
+				<div className='content-body'>
+					<BrowserRouter>
+						<Switch>
+							<Route path='/home' component={Home} />
+							<Route path='/shop/category/product/:product_id' component={Product} />
+							<Redirect exact path='/' to='/home' />
+						</Switch>
+					</BrowserRouter>
+				</div>
+
+				<footer>&#169; Copyright 2020 | ABCD Gold Shop | Contact: kvigneshwaranit@gmail.com</footer>
+			</div>
+		</Fragment>
+	);
 }
 
 export default App;
