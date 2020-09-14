6066b1923de2c25013ba6a4313661cd62b7547f1
Vigneshwaran Kannan <kvigneshwaranit@gmail.com>
2020-09-14 05:32:31 +0530

    react modal component

--- e-commerce/src/sharedComponents/SharedModal/index.js
+++ e-commerce/src/sharedComponents/SharedModal/index.js
@@ -1,61 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
- 
+import Button from '../../sharedComponents/Button';
+
+import './index.css';
+
 const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
-  }
+	content: {
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		marginRight: '-50%',
+		transform: 'translate(-50%, -50%)',
+	},
 };
- 
+
 // Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
-Modal.setAppElement('#root')
- 
-function SharedModal(){
-  var subtitle;
-  const [modalIsOpen,setIsOpen] = React.useState(false);
-  function openModal() {
-    setIsOpen(true);
-  }
- 
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00';
-  }
- 
-  function closeModal(){
-    setIsOpen(false);
-  }
- 
-    return (
-      <div>
-        <button onClick={openModal}>Open Modal</button>
-        <Modal
-          isOpen={modalIsOpen}
-          onAfterOpen={afterOpenModal}
-          onRequestClose={closeModal}
-          style={customStyles}
-          contentLabel="Example Modal"
-        >
- 
-          <h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2>
-          <button onClick={closeModal}>close</button>
-          <div>I am a modal</div>
-          <form>
-            <input />
-            <button>tab navigation</button>
-            <button>stays</button>
-            <button>inside</button>
-            <button>the modal</button>
-          </form>
-        </Modal>
-      </div>
-    );
+Modal.setAppElement('#root');
+
+function SharedModal(props) {
+	const [modalIsOpen, setIsOpen] = useState(false);
+	function openModal() {
+		setIsOpen(true);
+	}
+
+	function afterOpenModal() {}
+
+	function closeModal() {
+		setIsOpen(false);
+	}
+
+	return (
+		<div>
+			<Button btnName={props.modalBtnName || 'Click Me'} btnClick={() => openModal()} btnClassName={props.modalBtnClassName} />
+			<Modal
+				isOpen={modalIsOpen}
+				onAfterOpen={afterOpenModal}
+				onRequestClose={closeModal}
+				style={customStyles}
+				contentLabel='Example Modal'
+			>
+				<button onClick={closeModal} className='modal-close-btn'>
+					x
+				</button>
+				<div className='under-dev'>Under Development!!</div>
+			</Modal>
+		</div>
+	);
 }
- 
+
 export default SharedModal;
